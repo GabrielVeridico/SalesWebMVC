56d686fe810630d29d2af1361aa432e7350e94fb
Gabriel <gabrielvmarra1@hotmail.com>
2022-07-24 11:28:44 -0300

    Creation of View-Model-Controller Department

---
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var departmentList = new List<Department>();
+            departmentList.Add(new Department { Id = 1, Name = "Eletronics" });
+            departmentList.Add(new Department { Id = 2, Name = "Fashion" });
+
+            return View(departmentList);
+        }
+    }
+}
